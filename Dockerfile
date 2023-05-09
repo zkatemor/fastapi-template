ac97bd10e5cbfcbde31be5385b63d80bc94b9360
zkatemor <ea.zababurina@gmail.com>
2023-05-09 20:11:36 +0300

    alembic config

--- Dockerfile
+++ Dockerfile
@@ -3,10 +3,10 @@ FROM python:3.9
 ENV PYTHONDONTWRITEBYTECODE 1
 ENV PYTHONUNBUFFERED 1
 
-WORKDIR /app/
+WORKDIR /app
+COPY ./requirements.txt /app/requirements.txt
 
-RUN pip install -r requirements
-
-COPY . /app/
+RUN pip install --no-cache-dir --upgrade -r /app/requirements.txt
+COPY . /app
 
 EXPOSE 8000
--- alembic/env.py
+++ alembic/env.py
@@ -5,9 +5,20 @@ from sqlalchemy import pool
 
 from alembic import context
 
+import os, sys
+from dotenv import load_dotenv
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+load_dotenv(os.path.join(BASE_DIR, ".env"))
+sys.path.append(BASE_DIR)
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+# this will overwrite the ini-file sqlalchemy.url path
+# with the path given in the config of the main code
+config.set_main_option("sqlalchemy.url", os.environ["DATABASE_URL"])
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -18,7 +29,10 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+
+from app import models
+
+target_metadata = models.Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
